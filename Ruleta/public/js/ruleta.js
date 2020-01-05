f1d65c33dc2fbd50b1d63a8dee710788759c10f5
Agustina Maraz <agus_maraz@hotmail.com>
2020-01-05 20:58:42 -0300

    beta

--- Ruleta/public/js/ruleta.js
+++ Ruleta/public/js/ruleta.js
@@ -1,5 +1,42 @@
-// let cuerpo = document.body;
-// let boton = document.querySelector('.start');
-// console.log(boton);
-// boton.addEventListener('click', )
-// var miRuleta = new Winwheel();
+let ruleta = new Winwheel({
+    'numSegments': 6,
+    'outerRadius': 170,
+    'segments': [
+        { 'fillStyle': '#9ADDE8', 'text': 'Milanesas' },
+        { 'fillStyle': '#9AC5E8', 'text': 'Sopa' },
+        { 'fillStyle': '#9AAEE8', 'text': 'Pastel de papa' },
+        { 'fillStyle': '#CD9AE8', 'text': 'Pizza' },
+        { 'fillStyle': '#E89AC7 ', 'text': 'Guiso' },
+        { 'fillStyle': '#E89A9A  ', 'text': 'Asado' },
+    ],
+    'animation': {
+        'type': 'spinToStop',
+        'duration': 3,
+        'spins': 2,
+        'callbackFinished': 'mensaje()',
+        'callbackAfter': 'dibujarIndicador()'
+    }
+});
+function mensaje() {
+    var elementoSeleccionado = ruleta.getIndicatedSegment();
+    Swal.fire('Te tocó ' +  elementoSeleccionado.text );
+    ruleta.stopAnimation(false);
+    ruleta.rotationAngle = 0;
+    ruleta.draw();
+    dibujarIndicador();
+};
+
+dibujarIndicador();
+function dibujarIndicador() {
+    var ctx = ruleta.ctx;
+    ctx.strokeStyle = 'navy';
+    ctx.fillStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(170, 0);
+    ctx.lineTo(230, 0);
+    ctx.lineTo(200, 40);
+    ctx.lineTo(171, 0);
+    ctx.stroke();
+    ctx.fill();
+};
--- Ruleta/resources/views/welcome.blade.php
+++ Ruleta/resources/views/welcome.blade.php
@@ -30,58 +30,15 @@
         @endif
         
     </div>
-    <section>
-        <input type="submit" onClick="ruleta.startAnimation();" value="girar">
+    <section class="caja">
+        <h1> Elige que vas a comer </h1>
+        <input type="submit" onClick="ruleta.startAnimation();" class="boton-girar" value="Girar">
+        <br>
         <canvas id="canvas" width="400" height="400"> </canvas>
     </section>
-    <script>
-        let ruleta = new Winwheel({
-            'numSegments': 6,
-            'outerRadius': 170,
-            'segments': [
-            {'fillStyle': '#94C6E3', 'text': 'hola' },
-            {'fillStyle': '#9AC5E8', 'text': 'amor' },
-            {'fillStyle': '#9AAEE8', 'text': 'mira' },
-            {'fillStyle': '#CD9AE8', 'text': 'lo' },
-            {'fillStyle': '#E89AC7 ', 'text': 'que' },
-            {'fillStyle': '#E89A9A  ', 'text': 'hice' }, 
-            ],
-            'animation': {
-                'type': 'spinToStop',
-                'duration': 5,
-                'spins': 8,
-                'callbackFinished': 'mensaje()',
-                'callbackAfter': 'dibujarIndicador()'
-            }
-        });
-        function mensaje(){
-            var elementoSeleccionado = ruleta.getIndicatedSegment();
-            console.log(elementoSeleccionado);
-            alert('Te tocó ' + elementoSeleccionado.text);
-            ruleta.stopAnimation(false);
-            ruleta.rotationAngle = 0;
-            ruleta.draw();
-            dibujarIndicador();
-        };
-
-        dibujarIndicador();
-            function dibujarIndicador() {
-                var ctx = ruleta.ctx;
-                ctx.strokeStyle = 'navy';
-                ctx.fillStyle = 'black';
-                ctx.lineWidth = 2;
-                ctx.beginPath();
-                ctx.moveTo(170, 0);
-                ctx.lineTo(230, 0);
-                ctx.lineTo(200, 40);
-                ctx.lineTo(171, 0);
-                ctx.stroke();
-                ctx.fill();
-            };
-            
-        </script>
-        
+ 
     </body>
     </html>
-    {{-- <script src="js/ruleta.js"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <script src="js/ruleta.js"></script>
     
